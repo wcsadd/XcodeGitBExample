1965d88a77edb484703e2a6143b271c78a444f09
Bill Sadd <Bill-User@imac.local>
2017-08-20 14:00:08 -0500

    Initial Commit

---
+++ XcodeGitBExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeGitBExample
+//
+//  Created by Bill Sadd on 8/20/17.
+//  Copyright © 2017 Bill Sadd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeGitBExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeGitBExample
+//
+//  Created by Bill Sadd on 8/20/17.
+//  Copyright © 2017 Bill Sadd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
